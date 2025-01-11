d4bd24f9c424de3c86489c221c1023ec83950625
ryan-hayward <rchayward99@gmail.com>
2025-01-11 11:46:41 -0600

    Chapter 16: Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+// page-specific metadata
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
 export default function Page() {
     return <p>Customers Page</p>;
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,8 +6,13 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
- 
+// page-specific metadata
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
+
 export default async function Page(props: {
     searchParams?: Promise<{
       query?: string;
